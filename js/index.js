fda7321044c4ddde78c87111292b90847edff9c4
Nikita <zhcoder@MacBook-Pro-Nikita-3.local>
2021-07-19 16:51:32 +1000

    last commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let game = new Game();
 game.generate(true);
 document.querySelector(".btnRestartGame").addEventListener('click', () => {
+    document.querySelector(".field").innerHTML = "";
     game.generate(true);
 });
